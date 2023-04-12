9990f087007983593be8feb88aec443963f0a6e9
seb99seb <sebvangpedersen@hotmail.dk>
2023-04-12 10:25:00 +0200

    please?

---
+++ SPA/src/App.js
@@ -0,0 +1,35 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import PopularMoviesList from "./components/popularMovieList";
+import HighestRatedMovieList from "./components/highestRatedMovieList"
+import MovieImg from './assets/Image/movie_black2.jpg';
+import SearchMovie from './components/searchMovie';
+import SearchedMovieList from './components/searchedMovieList';
+import Home from './components/home';
+
+function App() { 
+  return (
+    <div>
+      <div className="jumbotron pb-3 pt-3">
+        <div className="navbar navbar-expand-lg">
+          <nav className="nav navbar-nav">    
+            <Link to='/' className="nav-item nav-link">Home</Link>
+            <Link to='/popular' className="nav-item nav-link">Popular</Link>
+            <Link to='/highest-rated' className="nav-item nav-link">Highest Rated</Link>
+            <Link to='/' className="nav-item nav-link">My favorite</Link>
+          </nav>
+        </div> 
+          <span className='h1'>React Moviefinder <img className="rounded movie_img m-3" src={MovieImg} width="75" height="75"/></span>
+      <span className="d-flex justify-content-between p-0">This small App demonstrates React, Redux-Toolkit, RTK Query and React-Router<SearchMovie/></span>
+        </div>
+        <Routes> 
+            <Route path='/' element={<Home/>} />   
+            <Route path='/popular' element={<PopularMoviesList/>} />     
+            <Route path='/highest-rated' element={<HighestRatedMovieList/>} />
+            <Route path='/searchedMovie' element={<SearchedMovieList/>} /> 
+        </Routes>
+
+    </div>
+  );
+}
+
+export default App;
